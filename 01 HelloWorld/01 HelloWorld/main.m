e1fa01d838b91df388040947f2d92df8e641f8ac
ElinaSamardjieva <elina.sm2@gmail.com>
2016-03-12 11:02:37 +0200

    Creating Hello World program

---
+++ 01 HelloWorld/01 HelloWorld/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  01 HelloWorld
+//
+//  Created by Elina on 3/12/16.
+//  Copyright © 2016 Elina. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSLog(@"Hello, World!");
+        
+        int a = 50;
+        int b = 25;
+        
+        int sum = a + b;
+        
+        NSLog(@"The sum of %i and %i is ---> %i.", a, b, sum);
+    }
+    return 0;
+}
