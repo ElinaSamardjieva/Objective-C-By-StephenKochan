e5a442bae01ababe8db94246604161b61f567455
ElinaSamardjieva <elina.sm2@gmail.com>
2016-03-13 16:34:53 +0200

    Example for getters and setters with countries and capitals added

---
+++ 03 Getters and Setters/03 Getters and Setters/main.m
@@ -0,0 +1,66 @@
+//
+//  main.m
+//  03 Getters and Setters
+//
+//  Created by Elina on 3/13/16.
+//  Copyright © 2016 Elina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Country: NSObject
+
+-(void) setName: (NSString*) na;
+-(void) setCapital: (NSString*) ca;
+-(NSString*) name;
+-(NSString*) capital;
+-(void)print;
+
+@end
+
+@implementation Country
+{
+    NSString *name;
+    NSString *capital;
+}
+
+
+-(void) setName:(NSString *)na
+{
+    name = na;
+}
+
+-(void) setCapital:(NSString *)ca
+{
+    capital = ca;
+}
+
+-(NSString*) name
+{
+    return name;
+}
+
+-(NSString*) capital
+{
+    return capital;
+}
+
+-(void) print
+{
+    NSLog(@"The capital of %@ is %@.", name, capital);
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    
+        Country *cuba = [[Country alloc]init];
+        [cuba setName: @"Japan"];
+        [cuba setCapital: @"Tokyo"];
+        
+        [cuba print];
+        
+    }
+    return 0;
+}
